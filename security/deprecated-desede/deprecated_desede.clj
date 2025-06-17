538f5704a395adc023770f1b2a79cab1073e2fb2
Sam Umbach <samumbach@gmail.com>
2025-06-16 21:50:41 -0400

    fixup! MAYBE FIXUP

--- security/deprecated-desede/deprecated_desede.clj
+++ security/deprecated-desede/deprecated_desede.clj
@@ -4,7 +4,7 @@
            (javax.crypto.spec SecretKeySpec IvParameterSpec)))
 
 (defn encrypt-vulnerable [data key iv]
-  (let [cipher (Cipher/getInstance "DESEDERFC3211WRAP")]
+  (let [cipher (Cipher/getInstance "DESEDERFC3217WRAP")]
     (.init cipher Cipher/ENCRYPT_MODE (SecretKeySpec. key "DESede") (IvParameterSpec. iv))
     (.encodeToString (Base64/getEncoder) (.doFinal cipher data))))
 
